93b75e36c4ecb3dd84068ccd24c7015cd7a98676
hirushapraveen <112403631+hirushapraveen@users.noreply.github.com>
2024-07-17 12:32:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,25 +3,25 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029Va5h771DeONE7tZmdc3R'
+global.linkGroup = 'https://chat.whatsapp.com/DNjwchXAm6NE4fVn1fRN3p'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "HP";
+global.owner = ['94754431282'];
+global.premium = ['94754431282'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "SL Downloads";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
+global.available = true
+global.autoTyping = true
+global.autoRecord = true
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
